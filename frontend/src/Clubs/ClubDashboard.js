696253396e94c7e9a7b1d07806f34fc8d3e21dbf
Connor Chase <cchase4210@gmail.com>
2025-03-26 19:45:17 -0700

    nice lttle box shadow for navbar

--- frontend/src/Clubs/ClubDashboard.js
+++ frontend/src/Clubs/ClubDashboard.js
@@ -11,14 +11,18 @@ function ClubDashboard() {
     return (
         <>
             <Navbar page={"Club Dashboard"} />
-            <Container fluid style={{backgroundColor: '#f7f5ed'}} className="vh-100  mt-0 p-4 flex-column">
+            <Container fluid style={{backgroundColor: '#fdfcf7'}} className="vh-100  mt-0 p-4 flex-column">
                 <Row className="align-items-start felx-grow-1 mb-3 text-center">
                     <Col className="p-3 mt-0 m-2 bg-light border border-dark-subtle text-dark rounded">
                         <p>
-                         columns are independent and stretch with the content
-                            inside it. see?
+                            __________________<br />
+                            |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />
+                            |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />
+                            |________________|<br /><br />
+                            cursor parking lot bitch
                         </p>
                     </Col>
+
                     <Col xs={6} className="p-3 mt-0 m-2 bg-light border border-dark-subtle text-dark rounded">
                         this column is wider. it will contain the
                     </Col>
