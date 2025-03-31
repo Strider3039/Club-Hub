e14106ff357aec7a2c56858ffa2856ff438d3dad
Eshaan <eshaan.singh09@gmail.com>
2025-03-31 14:39:57 -0700

    Update API endpoint URLs in Login and Register components to use environment variable

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -13,7 +13,7 @@ function Login() {
     e.preventDefault();
 
     try {
-        const response = await axios.post("http://127.0.0.1:8000/login/", {
+        const response = await axios.post(`${process.env.REACT_APP_API_BASE}/register/`, {
             username,
             password,
         });
--- frontend/src/Register/Register.js
+++ frontend/src/Register/Register.js
@@ -23,7 +23,7 @@ function Register() {
     }
 
     try {
-      const response = await axios.post("http://127.0.0.1:8000/register/", {
+      const response = await axios.post(`${process.env.REACT_APP_API_BASE}/register/`, {
         first_name: firstName,
         last_name: lastName,
         email,
