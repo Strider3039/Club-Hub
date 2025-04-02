2aba7add2ef9c3dd0772d6d3090b4ae3d601eea2
Connor Chase <cchase4210@gmail.com>
2025-04-02 16:53:42 -0700

    started connect friends frontend and backend

--- frontend/src/Friends/Friends.js
+++ frontend/src/Friends/Friends.js
@@ -1,15 +1,41 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import "./Friends.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import React { useState, useEffect} from "react";
 
 function Friends() {
-  const friendsList = [
-    { id: 1, name: "Ethan Olsen", club: "Python Club" },
-    { id: 2, name: "Connor Chase", club: "Frontend Club" },
-    { id: 3, name: "Trevin Cloy", club: "C++ Club" }
-  ];
 
-  return (
+    // friend object
+    const friend = {
+      id: int,
+      name: "",
+      clubs: []
+    }
+
+    const navigate = useNavigate();
+    // const [friends, setFriends] = React.useState([]); // this will contain a list of friend objects
+
+    // this function is only called when the page initially loads
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user){
+            // make sure the user is logged in, then continue fetching friends list
+            const friends = JSON.parse(localStorage.getItem("friends"));
+
+
+        }
+        else
+        {
+            setError("you must be logged in to have friends lamo ┏ (゜ω゜)=👉");
+        }
+    }, []);
+
+
+
+
+    return (
     <div className="friends-page">
       <NavBar page="Friends" />
       <div className="friends-container">
@@ -23,7 +49,7 @@ function Friends() {
         </ul>
       </div>
     </div>
-  );
+    );
 }
 
 export default Friends;
