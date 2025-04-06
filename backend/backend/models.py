ae2762f207947926f8fe245754a3452082c9b7db
Ethan <strider3039@gmail.com>
2025-04-06 15:02:41 -0700

    Users can no longer create multiple clubs

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -14,6 +14,7 @@ class CustomUser(AbstractUser):
 class Club(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
+    creator = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='created_club', null=True)
     members = models.ManyToManyField(CustomUser, related_name='clubs')
     officers = models.ManyToManyField(CustomUser, related_name='club_officers', blank=True)
 
