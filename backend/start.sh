f0ffc7370893c8ad09eb3024a411d6abdc616179
Eshaan <eshaan.singh09@gmail.com>
2025-04-06 15:38:09 -0700

    Fix variable expansion in Gunicorn command in Dockerfile and start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,4 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Starting Gunicorn server..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT} --log-file -
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
