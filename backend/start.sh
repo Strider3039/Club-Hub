33f55bd0e965306c240fdd263d524e42c93a6066
Eshaan <eshaan.singh09@gmail.com>
2025-04-06 14:52:33 -0700

    Update Dockerfile and start script to use Gunicorn and dynamic port

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,5 @@
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
+echo "Starting Gunicorn server..."
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
