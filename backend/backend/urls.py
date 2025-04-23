26ca438b364bf95987bfd3adbc6eba26aa42995b
Ethan <strider3039@gmail.com>
2025-04-23 13:50:45 -0700

    started announcement features

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('clubs/delete/<int:club_id>/', views.ClubDeleteView.as_view(), name='club-delete'),
     path('clubs/update/<int:club_id>/', views.ClubUpdateView.as_view(), name='club-update'),
     path('clubs/<int:club_id>/', views.ClubDetailView.as_view(), name='club-detail'),
+    # path('clubs/announcements/<int:club_id>/', views.AnnouncementListView.as_view(), name='club-announcements'),
 
     # Friends URLs
     path('friends/', FriendListView.as_view(), name='friend-list'),
