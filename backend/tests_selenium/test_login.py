cf2395643f67e993cf529f173bbd54ea44c835fa
Trevin Cloy <trevin.cloy@wsu.edu>
2025-04-25 14:44:05 -0700

    Tests are passing.

--- backend/tests_selenium/test_login.py
+++ backend/tests_selenium/test_login.py
@@ -2,18 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 import time
 import tempfile
-import random
 
 temp_user_data_dir = tempfile.mkdtemp()
 
 options = Options()
 options.binary_location = "/usr/bin/chromium-browser"
-options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
@@ -31,17 +27,12 @@ try:
     username = "testlogin"
     password = "pass"
 
-    print(f"Url1: {driver.current_url}")
-
     driver.find_element(By.XPATH, '//input[@placeholder="Username"]').send_keys(username)
-    driver.find_element(By.XPATH, '//input[@placeholder="Password"]').send_keys(password, Keys.ENTER)
+    driver.find_element(By.XPATH, '//input[@placeholder="Password"]').send_keys(password)
 
-    print(f"Url2: {driver.current_url}")
+    driver.find_element(By.XPATH, '//button[text()="Login"]').click()
 
-    #driver.find_element(By.XPATH, '//button[text()="Login"]').click()
-
-    
-    #print(f"Url3: {driver.current_url}")
+    time.sleep(5)
 
     if "/home" in driver.current_url:
         print(f"Registration successful.")
--- backend/tests_selenium/test_register.py
+++ backend/tests_selenium/test_register.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
@@ -13,7 +12,6 @@ temp_user_data_dir = tempfile.mkdtemp()
 
 options = Options()
 options.binary_location = "/usr/bin/chromium-browser"
-options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
@@ -32,35 +30,20 @@ try:
     username = f"testuser{random_number}"
     email = f"{username}@test.com"
 
-    print(f"Url1: {driver.current_url}")
-    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "First Name")))
-
     driver.find_element(By.XPATH, '//input[@placeholder="First Name"]').send_keys("Test")
     driver.find_element(By.XPATH, '//input[@placeholder="Last Name"]').send_keys("User")
     driver.find_element(By.XPATH, '//input[@placeholder="Email"]').send_keys(email)
-    dob_input = driver.find_element(By.XPATH, '//input[@type="date"]')
-    driver.execute_script("arguments[0].value = '01-01-2000';", dob_input)
-    #driver.find_element(By.XPATH, '//input[@placeholder="Date of Birth"]').send_keys("01/01/2000")
+    driver.find_element(By.XPATH, '//input[@placeholder="Date of Birth"]').send_keys("01/01/2000")
     driver.find_element(By.XPATH, '//input[@placeholder="Username"]').send_keys(username)
     driver.find_element(By.XPATH, '//input[@placeholder="Password"]').send_keys("TestPass123!")
-    driver.find_element(By.XPATH, '//input[@placeholder="Confirm Password"]').send_keys("TestPass123!", Keys.ENTER)
+    driver.find_element(By.XPATH, '//input[@placeholder="Confirm Password"]').send_keys("TestPass123!")
 
-    print(f"Url2: {driver.current_url}")
-
-    #register_button = WebDriverWait(driver, 10).until(
-    #EC.element_to_be_clickable((By.XPATH, '//button[text()="Register"]'))
-    #)
-    #register_button.click()
-
-    #register_button = driver.find_element(By.XPATH, '//button[text()="Register"]')
-    #driver.execute_script("arguments[0].click();", register_button)
-
-    #print(f"Url3: {driver.current_url}")
+    driver.find_element(By.XPATH, '//button[text()="Register"]').click()
+    time.sleep(5)
 
     if "/login" in driver.current_url:
         print(f"Registration successful!\nUsername: {username}\nEmail: {email}")
     else:
-        #print(f"Registration might have failed.\nUsername: {username}\nEmail: {email}")
         try:
             error_element = driver.find_element(By.CLASS_NAME, "error")
             print(f"Registration failed with error: {error_element.text}")
